3ed96d0526c4b7567f01582b2c63220ca095619b
Facundo Veron <facu_ve_2016@hotmail.com>
2022-10-05 21:55:54 -0300

    Ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
-const { src, dest, watch , series, parallel } = require('gulp');
+const { src, dest, watch , parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
 const postcss    = require('gulp-postcss')
 const sourcemaps = require('gulp-sourcemaps')
 const cssnano = require('cssnano');
-const concat = require('gulp-concat');
 const terser = require('gulp-terser-js');
-const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin'); // Minificar imagenes 
 const notify = require('gulp-notify');
 const cache = require('gulp-cache');
-const clean = require('gulp-clean');
 const webp = require('gulp-webp');
 
 const paths = {
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
+// Seleccionamos el boton y el sidebar
 const mobileMenuBtn = document.querySelector('#mobile-menu');
 const sidebar = document.querySelector('.sidebar');
 
+// Si el boton existe creamos el menu
 if (mobileMenuBtn) {
     mobileMenuBtn.addEventListener('click', function () {
         sidebar.classList.toggle('mostrar');
