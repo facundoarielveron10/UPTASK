5a022eadb42a98b79343c7e857c6469edc59d0ff
Facundo Veron <facu_ve_2016@hotmail.com>
2022-10-06 16:29:37 -0300

    Responsive: Ajustes

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,16 @@ const body = document.querySelector('body');
 // Si el boton existe creamos el menu
 if (mobileMenuBtn) {
     mobileMenuBtn.addEventListener('click', function () {
-        sidebar.classList.toggle('mostrar');
+        if (sidebar.classList.contains('mostrar')) {
+            sidebar.classList.add('ocultar');
+            setTimeout(() => {
+                sidebar.classList.remove('mostrar');
+                sidebar.classList.remove(('ocultar'));
+            }, 500);
+        } else {
+            sidebar.classList.add("mostrar");
+            sidebar.classList.remove('ocultar');
+        }
         body.classList.toggle('overflow-hidden')
     });
 }
