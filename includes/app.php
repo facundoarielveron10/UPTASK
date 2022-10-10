f54d27b3584e0284d9e90d561ef17b1457e190e6
Facundo Veron <facu_ve_2016@hotmail.com>
2022-10-09 22:32:45 -0300

    Solucion?

--- includes/app.php
+++ includes/app.php
@@ -11,3 +11,13 @@ require 'database.php';
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
+
+// NO CORS
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
